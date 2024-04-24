d278a0598ac2404d1a2b9bae243824419de37758
JunYoung_Kim <hjklo0220@gmail.com>
2024-04-25 00:06:12 +0900

    add build frontend

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
+from starlette.responses import FileResponse
+from starlette.staticfiles import StaticFiles
 
 from api import question, answer, user
 
@@ -7,6 +9,7 @@ app = FastAPI()
 app.include_router(question.router)
 app.include_router(answer.router)
 app.include_router(user.router)
+app.mount("/assets", StaticFiles(directory="frontend/dist/assets"))
 
 origins = [
     "http://127.0.0.1:5173"
@@ -21,9 +24,5 @@ app.add_middleware(
 )
 
 @app.get("/")
-def health_check_handler():
-    return {"status": "ok"}
-
-@app.get("/hello")
-def hello_handler():
-    return {"message": "Hello World"}
+def index():
+    return FileResponse("frontend/dist/index.html")
